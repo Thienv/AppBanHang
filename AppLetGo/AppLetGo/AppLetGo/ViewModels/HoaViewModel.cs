20129f76fb560713c64d150c5e68d30b9ca2c94c
vongminhtan <45420102+vongminhtan@users.noreply.github.com>
2018-12-04 23:58:39 +0700

    asdf
    asdasdasd

---
+++ AppLetGo/AppLetGo/AppLetGo/ViewModels/HoaViewModel.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using AppLetGo.DAL;
+
+namespace AppLetGo.ViewModels
+{
+    class HoaViewModel: INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private ILoaiHoaRepository loaihoaRepository;
+
+        private IHoaRepository hoaRepository;
+        public void RaisePropertyChanged(string PropertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(PropertyName));
+        }
+        private Hoa hoa;
+        public Hoa Hoachon
+        {
+            get { return hoa; }
+            set
+            {
+                hoa = value;
+                RaisePropertyChanged("Hoachon");
+            }
+        }
+        public int Mahoa
+        {
+            get { return hoa.Mahoa; }
+            set
+            {
+                hoa.Mahoa = Mahoa;
+                RaisePropertyChanged("Mahoa");
+            }
+        }
+        public int Maloai
+        {
+            get { return hoa.Maloai; }
+            set
+            {
+                hoa.Maloai = value;
+                RaisePropertyChanged("Maloai");
+            }
+        }
+        public string Tenhoa
+        {
+            get { return hoa.Tenhoa; }
+            set
+            {
+                hoa.Tenhoa = value;
+                RaisePropertyChanged("Tenhoa");
+            }
+        }
+        public string Hinh
+        {
+            get { return hoa.Hinh; }
+            set
+            {
+                hoa.Hinh = value;
+                RaisePropertyChanged("Hinh");
+            }
+        }
+        public string Mota
+        {
+            get { return hoa.Mota; }
+            set
+            {
+                hoa.Mota = value;
+                RaisePropertyChanged("Mota");
+            }
+        }
+        public double Gia
+        {
+            get { return hoa.Gia; }
+            set
+            {
+                hoa.Gia = value;
+                RaisePropertyChanged("Gia");
+            }
+        }
+    }
+}
---
+++ AppLetGo/AppLetGo/AppLetGo/ViewModels/LoaiHoaViewModel.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Text;
+using System.Windows.Input;
+using AppLetGo.DAL;
+using Xamarin.Forms;
+
+namespace AppLetGo.ViewModels
+{
+    class LoaiHoaViewModel : INotifyPropertyChanged
+    {
+        private Loaihoa loaihoa;
+        ObservableCollection<Loaihoa> loaihoalist;
+        public ILoaiHoaRepository loaihoaRepository;
+        public ICommand AddLoaiHoa { get; private set; }
+        public ICommand UpdateLoaiHoa { get; private set; }
+        public ICommand DeleteLoaiHoa { get; private set; }
+        public ICommand SelectLoaihoa { get; private set; }
+        public LoaiHoaViewModel()
+        {
+            loaihoaRepository = new LoaiHoaRepository();
+            LoadLoaihoa();
+            AddLoaiHoa = new Command(Insert);
+            UpdateLoaiHoa = new Command(Update, CanExe);
+            DeleteLoaiHoa = new Command(Delete, CanExe);
+            SelectLoaihoa = new Command(ChonLoaiHoa);
+            loaihoa = new Loaihoa();
+        }
+
+        private void ChonLoaiHoa()
+        {
+
+        }
+
+        private void Delete()
+        {
+            loaihoaRepository.DeleteAsync(loaihoa.Maloai);
+            LoadLoaihoa();
+        }
+
+        private bool CanExe()
+        {
+            if (Loaihoa == null || Loaihoa.Maloai == 0)
+                return false;
+            else
+                return true;
+        }
+       
+        private void Update()
+        {
+            loaihoaRepository.Update(Loaihoa);
+            LoadLoaihoa();
+        }
+
+        public Loaihoa Loaihoa
+        {
+            get { return loaihoa; }
+            set
+            {
+                loaihoa = value;
+                RaisePropertyChanged("Loaihoa");
+                ((Command)UpdateLoaiHoa).ChangeCanExecute();
+
+            }
+        }
+        private void Insert()
+        {
+            loaihoaRepository.InsertAsync(loaihoa);
+            LoadLoaihoa();
+        }
+
+        public int ID
+        {
+            get { return loaihoa.Maloai; }
+            set
+            {
+                loaihoa.Maloai = value;
+                RaisePropertyChanged("ID");
+            }
+        }
+        public string Tenloai
+        {
+            get { return loaihoa.Tenloai; }
+            set
+            {
+                loaihoa.Tenloai = value;
+                RaisePropertyChanged("Tenloai");
+            }
+        }
+
+
+        public ObservableCollection<Loaihoa> Loaihoalist
+        {
+            get { return loaihoalist; }
+            set
+            {
+                loaihoalist = value;
+                RaisePropertyChanged("Loaihoalist");
+            }
+        }
+        void LoadLoaihoa()
+        {
+            Loaihoalist = loaihoaRepository.GetAll();
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void RaisePropertyChanged(string PropertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(PropertyName));
+        }
+    }
+}
